59e83a8bdea05a2cba12bf5b18a46377185d628a
shiqingshan <sqskg@163.com>
2020-01-15 15:59:02 +0800

    open feign client use

--- src/main/java/com/sting/consumer/ConsumerApplication.java
+++ src/main/java/com/sting/consumer/ConsumerApplication.java
@@ -2,12 +2,23 @@ package com.sting.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/sting/consumer/controller/HelloController.java
@@ -0,0 +1,38 @@
+package com.sting.consumer.controller;
+
+import com.sting.consumer.remoteService.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloRemote helloRemote;
+
+    @Autowired
+    private LoadBalancerClient client;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+    @GetMapping("/")
+    public String hello(@RequestParam String name){
+        name += "!";
+//        ServiceInstance instance = client.choose("EUREKA-PRODUCER");
+//
+//        String url = "http://" + instance.getHost() + ":" + instance.getPort() + "/hello/?name=" + name;
+//        System.out.println(url);
+//        String url = "http://EUREKA-PRODUCER/hello/?name=" + name;
+//        System.out.println(url);
+//        return restTemplate.getForObject(url,String.class);
+        return helloRemote.hello(name);
+    }
+}
