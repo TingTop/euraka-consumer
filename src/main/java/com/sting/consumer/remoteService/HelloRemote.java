00255301501e29937d2b186b657e7b86e1924c36
shiqingshan <sqskg@163.com>
2020-01-15 17:50:21 +0800

    add openFeign future

--- src/main/java/com/sting/consumer/remoteService/HelloRemote.java
+++ src/main/java/com/sting/consumer/remoteService/HelloRemote.java
@@ -1,10 +1,11 @@
 package com.sting.consumer.remoteService;
 
+import com.sting.consumer.hystrix.HelloRemoteHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "eureka-producer")
+@FeignClient(name = "eureka-producer",fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
     @GetMapping("/hello/")
